b357fa297f21eb5e54c25a8a6cb547f987cb88c3
Ankita Varat <159252485+ankitavarat@users.noreply.github.com>
2026-07-18 16:21:21 +0530

    Update service worker to include smart_farmer_ui.html

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'shetkarimitra-v1';
 const ASSETS = [
   './',
-  './index.html',
+  './smart_farmer_ui.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
@@ -12,16 +12,16 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting()) // लगेच ॲक्टिव्हेट करण्यासाठी
+    }).then(() => self.skipWaiting()) 
   );
 });
 
-// सर्विस वर्कर ॲक्टिव्हेट करणे
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
 });
 
-// नेटवर्कवरून डेटा मिळवणे
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
