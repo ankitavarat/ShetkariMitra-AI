46ffa0d98ebd943782ca05beea13b53ba67360ac
Ankita Varat <159252485+ankitavarat@users.noreply.github.com>
2026-08-18 11:25:10 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1190,7 +1190,7 @@ def chatbot_response(question):
         """
 
         response = groq_client.chat.completions.create(
-          model="llama-3.3-70b-versatile",
+          model="openai/gpt-oss-120b",
           messages=[
             {
               "role": "system",
